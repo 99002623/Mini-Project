94ab709ee34992f9d6fb0ef6b4e503db1275fb48
Naga Akhil ES <nagaakhil.es@ltts.com>
2020-10-04 16:43:37 +0530

    update v2

--- Workbook/moneyRem_test.h
+++ Workbook/moneyRem_test.h
@@ -7,23 +7,18 @@ TEST(MoneyRemittance, DefaultConstructor) {
   EXPECT_EQ(0, c1.getMobileNum().length());
   EXPECT_EQ(0, c1.getCustomerID().length());
   EXPECT_EQ(0, c1.getWalletbal());
-}
-
-
-TEST(MoneyRemittance, ParametricConstructor) {
-  MoneyRemittance c1("Mr.Abc","9901063456",1000);
-  EXPECT_EQ(6, c1.getCustomerName().length());
-  EXPECT_EQ(10, c1.getMobileNum().length());
-  EXPECT_EQ(0, c1.getCustomerID().length());
-  EXPECT_EQ(1000, c1.getWalletbal());
+  //EXPECT_EQ(0, c1.getWalletbal());
 }
 
 TEST(MoneyRemittance, ParametricConstructor1) {
-  MoneyRemittance c1("Mr.Abc","9901063456","989Add225",1000);
+  MoneyRemittance c1("Mr.Abc","9901063456","989Add225",100010001,"SBI","SBIN00041152");
   EXPECT_EQ(6, c1.getCustomerName().length());
   EXPECT_EQ(10, c1.getMobileNum().length());
   EXPECT_EQ(9, c1.getCustomerID().length());
-  EXPECT_EQ(1000, c1.getWalletbal());
+  EXPECT_EQ(100010001,c1.getAccNumber());
+  EXPECT_EQ("SBI",c1.getBankName());
+  EXPECT_EQ("SBIN00041152",c1.getIfscCode());
+  EXPECT_EQ(0, c1.getWalletbal());
 }
 
 /*
@@ -33,23 +28,23 @@ TEST(MoneyRemittance, CreditTest) {
   EXPECT_EQ(500, c1.getWalletbal());
 }*/
 
-TEST(MoneyRemittance, CreditTest) {
-customerWallet *w_ptr;
-MoneyRemittance c1("Mr.Abc","9901063456",1000);
-w_ptr = &c1;
+TEST(MoneyRemittance, CreditTest) {  // Account Credit Test
 
+MoneyRemittance c1("Mr.Abc","9901063456","989Add225",100010001,"SBI","SBIN00041152");
+customerWallet *w_ptr = &c1;
+w_ptr->loadWallet(1000);  //Load Wallet
 w_ptr->credit(500);   // Dynamic binding
 EXPECT_EQ(500, w_ptr->getWalletbal());
 
 }
 
 
-TEST(MoneyRemittance, DebitTest) {
-customerWallet *w_ptr;
-MoneyRemittance c1("Mr.Abc","9901063456",1000);
-w_ptr = &c1;
+TEST(MoneyRemittance, DebitTest) {  // Account Credit Test
 
+MoneyRemittance c1("Mr.Abc","9901063456","989Add225",100010001,"SBI","SBIN00041152");
+customerWallet *w_ptr = &c1;
+//w_ptr->loadWallet(1000);  //Load Wallet
 w_ptr->debit(500);   // Dynamic binding
-EXPECT_EQ(1500, w_ptr->getWalletbal());
+EXPECT_EQ(500, w_ptr->getWalletbal());
 
 }
