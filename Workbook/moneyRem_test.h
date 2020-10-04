ddcba833e909bf10f5f762ee19acc062ad764cd3
Naga Akhil ES <nagaakhil.es@ltts.com>
2020-10-04 15:36:13 +0530

    .

--- Workbook/moneyRem_test.h
+++ Workbook/moneyRem_test.h
@@ -19,22 +19,35 @@ TEST(MoneyRemittance, ParametricConstructor) {
 }
 
 TEST(MoneyRemittance, ParametricConstructor1) {
-  customerWallet c1("Mr.Abc","9901063456","989Add225",1000);
+  MoneyRemittance c1("Mr.Abc","9901063456","989Add225",1000);
   EXPECT_EQ(6, c1.getCustomerName().length());
   EXPECT_EQ(10, c1.getMobileNum().length());
   EXPECT_EQ(9, c1.getCustomerID().length());
   EXPECT_EQ(1000, c1.getWalletbal());
 }
 
-
+/*
 TEST(MoneyRemittance, CreditTest) {
   MoneyRemittance c1("Mr.Abc","9901063456",1000);
   c1.credit(500);
   EXPECT_EQ(500, c1.getWalletbal());
+}*/
+
+TEST(MoneyRemittance, CreditTest) {
+customerWallet *w_ptr;
+MoneyRemittance c1("Mr.Abc","9901063456",1000);
+w_ptr = &c1;
+
+w_ptr->credit(500);   // Dynamic binding
+EXPECT_EQ(500, w_ptr->getWalletbal());
+
 }
+
 
 TEST(MoneyRemittance, DebitTest) {
   MoneyRemittance c1("Mr.Abc","9901063456",1000);
   c1.debit(500);
   EXPECT_EQ(1500, c1.getWalletbal());
 }
+
+
