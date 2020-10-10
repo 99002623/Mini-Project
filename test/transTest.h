54e3abe1bfa56d2ae91983cdaa709a4ddf985675
Naga Akhil ES <nagaakhil.es@ltts.com>
2020-10-10 17:22:25 +0530

    added one more test case

--- test/transTest.h
+++ test/transTest.h
@@ -3,21 +3,20 @@
 
 #include "../src/transaction.h"
 #include <gtest/gtest.h>
-//#include "importDataset.h"
 
-/*
+
 TEST(Transaction, sendMoney) {
 
 Transaction t1;
 
-t1.addCustomer("Mr.Abc","9900112205","989Add225","100010001","SBI","SBIN00041152",5000);
+t1.addCustomer("Mr.Abc","9900112255","989Add225","100010001","SBI","SBIN00041152",5000);
 
-bool t_flag = t1.sendMoney("9900112205",1000);
+bool t_flag = t1.sendMoney("9900112255",1000);
 
   EXPECT_EQ(true, t_flag);
   EXPECT_EQ(6000,t1.r_accBal);
   EXPECT_EQ(0,t1.r_wBal);
-}*/
+}
 
 TEST(Transaction, sendMoneyDB) {
 
