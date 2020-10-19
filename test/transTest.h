d2cf6ec9e5b7402989ec178a9a06d39950ce0e89
Naga Akhil ES <nagaakhil.es@ltts.com>
2020-10-19 18:29:55 +0530

    fix:duplicate,prefix inc

--- test/transTest.h
+++ test/transTest.h
@@ -16,6 +16,17 @@ t1.sendMoney("9900112255",1000);
   EXPECT_EQ(0,t1.r_wBal);
 }
 
+TEST(Transaction, withdrawMoney) {
+
+Transaction t1;
+
+t1.addCustomer("Mr.Abc","9900112205","989Add225","100010001","SBI","SBIN00041152",5000);
+ t1.withdrawMoney("9900112205",500);
+  EXPECT_EQ(4500,t1.r_accBal);
+  EXPECT_EQ(500,t1.r_wBal);
+
+}
+
 TEST(Transaction, sendMoneyDB) {
 
 Transaction t1;
@@ -27,14 +38,15 @@ t1.importDataset("bankDatabase.csv");
 
 }
 
-TEST(Transaction, withdrawMoney) {
+
+TEST(Transaction, withdrawMoneyDB) {
 
 Transaction t1;
-
-t1.addCustomer("Mr.Abc","9900112205","989Add225","100010001","SBI","SBIN00041152",5000);
- t1.withdrawMoney("9900112205",500);
-  EXPECT_EQ(4500,t1.r_accBal);
-  EXPECT_EQ(500,t1.r_wBal);
+t1.importDataset("bankDatabase.csv");
+//t1.addCustomer("Mr.Abc","9900112255","989Add225","100010001","SBI","SBIN00041152",5000);
+ t1.withdrawMoney("9900112205",1000);
+  EXPECT_EQ(2000,t1.r_accBal);
+  EXPECT_EQ(1000,t1.r_wBal);
 
 }
 
